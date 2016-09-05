35ad46a8943a7e8a18851fb8f6b34b3f8b8e4711
ekamal <ekamal.corps@gmail.com>
2016-09-05 13:38:48 +0000

    Project 1.6.2 - Tracking
    - Ajout de la creation d'un champ campagne dans le serveur de tracking

--- src/EK/MailBundle/Controller/DefaultController.php
+++ src/EK/MailBundle/Controller/DefaultController.php
@@ -350,12 +350,31 @@ class DefaultController extends Controller
             $em->flush();
 
 
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, "http://newofferz4u.com/addCamp.php?id=".$campagne->getId()."");
-            curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $fields = array(
+                'id' => $campagne->getId(),
+                'lienOffre' => urlencode($campagne->getOffre()->getLien()),
+                'lienImage' => urlencode($campagne->getOffre()->getCreative()),
+                'lienUnsub' => urlencode("http://newofferz4u.com/unsublist")
+            );
+            $fields_string = '';
+            $url = 'http://newofferz4u.com/addCamp.php';
 
-            $output = curl_exec($ch);
+            foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+            rtrim($fields_string, '&');
+
+
+            //open connection
+            $ch = curl_init();
+
+            //set the url, number of POST vars, POST data
+            curl_setopt($ch,CURLOPT_URL, $url);
+            curl_setopt($ch,CURLOPT_POST, count($fields));
+            curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+
+            //execute post
+            $result = curl_exec($ch);
+
+            //close connection
             curl_close($ch);
 
             $chemin = $this->container->get('kernel')->getRootdir().'/../web/logs/';
